78ad351e158d658059686ccea99199bd3f17d64b
YoussefHassan4002 <moazz784@users.noreply.github.com>
2026-03-10 12:12:43 +0200

    Update binary files and assembly information for LeafScan API, Application, Domain, and Infrastructure projects
    This commit includes updates to various binary files (.dll, .pdb) across the LeafScan API, Application, Domain, and Infrastructure projects. Additionally, the AssemblyInformationalVersion attributes in the AssemblyInfo.cs files have been updated to reflect the new versioning scheme. The sourcelink.json files have also been modified to point to the latest commit in the repository.

--- backend/LeafScan.Infrastructure/obj/Debug/net8.0/LeafScan.Infrastructure.AssemblyInfo.cs
+++ backend/LeafScan.Infrastructure/obj/Debug/net8.0/LeafScan.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeafScan.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+744a06f8430999765d6fe5de1d483dab59cda90d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dca4b36ce0e5b90ee78ed50b1a09e4f1c9ee1754")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeafScan.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeafScan.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
